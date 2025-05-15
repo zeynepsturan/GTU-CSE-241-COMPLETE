cfef1be2d67b057437c3052d11146f0beea409eb
Zeynep Sude Turan <157915520+zeynepsturan@users.noreply.github.com>
2025-05-15 23:17:36 +0300

    Update main.cpp

--- PS 7/main.cpp
+++ PS 7/main.cpp
@@ -3,40 +3,43 @@
 #include "Ant.h"
 #include "Doodlebug.h"
 using namespace std;
+
 int main(){
-  
-  srand(time(NULL)); // Seed random number generator
-  World w;
-  string s;
-  
-  // Randomly create 100 ants
-  int antcount = 0;
-  while(antcount<INITIALANTS){
-    int x=rand()%WORLDSIZE;
-    int y=rand()%WORLDSIZE;
-    if(w.getAt(x,y)==NULL){ // Only put ant in empty spot
-      antcount++;
-      Ant *a1 = new Ant(&w,x,y);
-    }
-  }
-  
-  // Randomly create 5 doodlebugs
-  int doodlecount=0;
-  while (doodlecount<INITIALBUGS){
-    int x=rand()%WORLDSIZE;
-    int y=rand()%WORLDSIZE;
-    if (w.getAt(x,y)==NULL){ // Only put doodlebug in empty spot
-      doodlecount++;
-      Doodlebug *d1 = new Doodlebug(&w,x,y);
-    }
-  }
-  
-  // Run simulation forever, until user cancels
-  while (true){
-    w.Display();
-    w.SimulateOneStep();
-    cout<<endl<<"Press enter for next step"<<endl;
-    getline(cin,s);
-  }
-  return 0;
+	srand(time(NULL)); // Seed random number generator
+	World w;
+	string s;
+
+	// Randomly create 100 ants
+	int antcount = 0;
+
+	while(antcount<INITIALANTS){
+		int x=rand()%WORLDSIZE;
+		int y=rand()%WORLDSIZE;
+		if(w.getAt(x,y)==NULL){ // Only put ant in empty spot
+		antcount++;
+		Ant *a1=new Ant(&w,x,y);
+	}
+}
+
+	// Randomly create 5 doodlebugs
+	int doodlecount=0;
+	
+	while (doodlecount<INITIALBUGS){
+		int x=rand()%WORLDSIZE;
+		int y=rand()%WORLDSIZE;
+		if (w.getAt(x,y)==NULL){ // Only put doodlebug in empty spot
+		doodlecount++;
+		Doodlebug *d1=new Doodlebug(&w,x,y);
+		}
+	}
+
+	// Run simulation forever, until user cancels
+	while (true){
+		w.Display();
+		w.SimulateOneStep();
+		cout<<endl<<"Press enter for next step"<<endl;
+		getline(cin,s);
+	}
+
+	return 0;
 }
